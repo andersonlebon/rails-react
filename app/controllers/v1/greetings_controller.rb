7e301831b43028d4d80e579a42dd660c0ba65781
Caleb Anderson <buyananderson@gmail.com>
2021-12-20 23:21:34 +0200

    Correct linter errors 2

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 module V1
-  class GreetingsController < ApplicationController # rubocop:todo Style/Documentation
+  class GreetingsController < ApplicationController
     def index
       @messages = Message.find(rand(1..Message.all.size))
       render json: @messages
